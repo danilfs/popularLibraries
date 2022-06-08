ad3890bcf40840ec46ea3025c95a62fa014124d5
danilfs009@yandex.ru <62214743+danilfs@users.noreply.github.com>
2022-06-08 17:32:15 +0300

    add layout

--- app/src/main/java/com/example/gitapp/MainActivity.kt
+++ app/src/main/java/com/example/gitapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import com.example.gitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate (savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.refreshButton.setOnClickListener {
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        }
     }
-}
+    }
+
+
+
+
+
+
