63019917f61f71bead25102db65f4cd0007497cb
danilfs009@yandex.ru <62214743+danilfs@users.noreply.github.com>
2022-06-27 09:25:13 +0300

    user layout and dependecies

---
+++ app/src/main/java/com/example/gitapp/ui/users/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.gitapp.ui.users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gitapp.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/gitapp/ui/users/UsersFragment.kt
@@ -0,0 +1,7 @@
+package com.example.gitapp.ui.users
+
+import androidx.fragment.app.Fragment
+import com.example.gitapp.R
+
+class UsersFragment : Fragment(R.layout.fragment_users) {
+}
